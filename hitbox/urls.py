ae3e54e4aab7b34ca651cae6eca7312ad7a92ff0
SethTanoue <71104304+SethTanoue@users.noreply.github.com>
2022-07-02 03:22:09 -1000

    No database

--- hitbox/urls.py
+++ hitbox/urls.py
@@ -5,6 +5,6 @@ app_name = 'hitbox'
 
 urlpatterns = [
     path('', views.leaderboard, name='welcome'),
-    path('', views.leaderboard, name='welcome'),
-    path('', views.leaderboard, name='welcome'),
+    path('game', views.game, name='game'),
+    path('leaderboard', views.result, name='results'),
 ]
--- hitbox/views.py
+++ hitbox/views.py
@@ -5,4 +5,12 @@ from .models import Leaderboard
 
 def leaderboard(request):
     leaderboard = Leaderboard.objects.all()
-    return render(request, {'leaderboard': leaderboard})
+    return render(request, 'hitbox/index.html', {'leaderboard': leaderboard})
+
+def game(request):
+    leaderboard = Leaderboard.objects.all()
+    return render(request, 'hitbox/game.html', {'leaderboard': leaderboard})
+
+def result(request):
+    leaderboard = Leaderboard.objects.all()
+    return render(request, 'hitbox/leaderboards.html', {'leaderboard': leaderboard})
